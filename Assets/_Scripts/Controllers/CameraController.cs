c36bfd4afb5512f4e1d36e671a2995574f741cb8
Cepe6 <vmladenov22@gmail.com>
2017-10-22 01:41:24 +0300

    Player model and movement integration

---
+++ Assets/_Scripts/Controllers/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    private GameObject _player;
+    [SerializeField]
+    private float _cameraHeight = 20.0f;
+    [SerializeField]
+    private float _cameraSpeed = 5f;
+    [SerializeField]
+    private float _xOffset;
+    [SerializeField]
+    private float _zOffset;
+
+    // Use this for initialization
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = _player.transform.position;
+        pos.y += _cameraHeight;
+        pos.x += _xOffset;
+        pos.z += _zOffset;
+        transform.position = Vector3.Lerp(transform.position, pos, _cameraSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/_Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    //Mouse button constants
+    private const int LEFT_CLICK = 0;
+    private const int RIGHT_CLICK = 1;
+    private const int MIDDLE_CLICK = 2;
+
+    //Player movement variables
+    [SerializeField]
+    private float _movementSpeed = 5f;
+
+    private Rigidbody _rigidbody;
+    private Quaternion _lastRotation = new Quaternion(0f, 0f, 0f, 0f);
+
+	// Use this for initialization
+	void Start () {
+        _rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+    }
+
+    //Executed every frame. The player movement function
+    private void Move() {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        if(x != 0f || z != 0f) { 
+            Vector3 movementDir = new Vector3(Mathf.CeilToInt(x), 0f, Mathf.CeilToInt(z));
+            transform.position += movementDir * _movementSpeed * Time.deltaTime;
+            transform.rotation = Quaternion.LookRotation(movementDir);
+        }
+    }
+}
+
